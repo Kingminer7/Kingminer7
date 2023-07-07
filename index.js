bf28000530d7e355d5bde98637e6bdfc216340ad
km7 dev <100500023+Kingminer7@users.noreply.github.com>
2023-07-07 14:36:17 -0400

    uh

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-var serveIndex = require('serve-index')
+const serveIndex = require('serve-index')
+const bodyParser = require('body-parser')
 
 const app = express();
 
@@ -7,10 +8,12 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/html/index.html');
 });
 
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/css', serveIndex(__dirname + '/static/css'));
 app.use('/js', serveIndex(__dirname + '/static/js'));
 app.use('/', express.static(__dirname + '/static/'));
+
+app.use('/form', require())
 
 app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/html/404.html');
